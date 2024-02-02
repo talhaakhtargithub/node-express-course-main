0a627a54741692c8aecce08eaaa3f4301e9cdf7f
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-02-02 15:02:40 +0500

    Comit

--- 01-node-tutorial/10-fs-sync.js
+++ 01-node-tutorial/10-fs-sync.js
@@ -10,3 +10,22 @@ writeFileSync(
 )
 console.log('done with this task')
 console.log('starting the next one')
+
+
+
+
+
+
+
+
+// Sure, here are some different flags that can be used with the `writeFileSync` method:
+// 1. `'w'`: Opens the file for writing. If the file already exists, its contents are overwritten. If the file does not exist, it's created.
+// 2. `'wx'`: Exclusive write. Similar to `'w'`, but it fails if the path exists.
+// 3. `'a'`: Opens the file for writing. If the file exists, the new data is appended to the end of the file. If the file does not exist, it's created.
+// 4. `'ax'`: Exclusive append. Similar to `'a'`, but it fails if the path exists.
+// 5. `'w+'`: Opens the file for reading and writing. If the file already exists, its contents are overwritten. If the file does not exist, it's created.
+// 6. `'r+'`: Opens the file for reading and writing. The file pointer is placed at the beginning of the file.
+// 7. `'a+'`: Opens the file for reading and appending. The file is created if it does not exist.
+// 8. `'wx+'`: Exclusive read/write. Similar to `'w+'`, but it fails if the path exists.
+// 9. `'ax+'`: Exclusive read/append. Similar to `'a+'`, but it fails if the path exists.
+// These flags provide different behaviors when writing to files based on whether the file already exists and whether exclusive access is required.
